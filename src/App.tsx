be853d6534c02877db2e9edd24b2c8be63643e77
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-06-10 22:42:10 +0800

    remove react-router-dom code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { HashRouter } from "react-router-dom";
 import Bottom from "./components/Bottom";
 import Top from "./components/Top";
 
 export default function App() {
     return (
         <main className="w-screen h-screen overflow-x-hidden flex flex-col">
-            <HashRouter>
-                <Top />
-            </HashRouter>
+            <Top />
 
             <Bottom />
         </main>
